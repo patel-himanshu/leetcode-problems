cc3464fc0d48e2be73dc2487d60fdab9e6477443
Himanshu Patel <hpatel27299@gmail.com>
2020-05-08 02:46:48 +0530

    Day 7 - May LeetCoding (Optimised)

--- May LeetCoding Challenge/cousins-in-binary-tree.py
+++ May LeetCoding Challenge/cousins-in-binary-tree.py
@@ -1,5 +1,3 @@
-# Question: https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3322/
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,55 +5,40 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def checkLevel(self, root, data, level):
+    # Function to find level of a node 
+    def getLevel(self, root, data, level):
         if root == None:
             return 0
         elif root.val == data:
             return level
         
-        currLevel = self.checkLevel(root.left, data, level+1)
+        currLevel = self.getLevel(root.left, data, level+1)
         if currLevel != 0:
             return currLevel
         
-        currLevel = self.checkLevel(root.right, data, level+1)
+        currLevel = self.getLevel(root.right, data, level+1)
         return currLevel
         
+    # Function to find the parent of a node
+    def findParent(self, root, parentData, data):
+        if root == None:
+            return 0
+        elif root.val == data:
+            return parentData
+        
+        currParent = self.findParent(root.left, root.val, data)
+        if currParent != 0:
+            return currParent
+        
+        currParent = self.findParent(root.right, root.val, data)
+        return currParent
+        
+    # Function to find whether given nodes are cousins or not
     def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
-        xLevel = self.checkLevel(root, x, 1)
-        yLevel = self.checkLevel(root, y, 1)
-        if xLevel == yLevel:
-            return True
-        else:
-            return False
-
-
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
-        visited = []
-        if root:
-            visited.append(root)
-        current = visited[0]
-        while current:
-            if current.left:
-                visited.append(current.left)
-                if current.left == x:
-                    a = 1
-                elif current.left == y:
-                    b = 1
-            elif current.right:
-                visited.append(current.right)
-                if current.right == x:
-                    a = 1
-                elif current.right == y:
-                    b = 1
-            visited.pop(0)
-            if not visited:
-                break
-            current = visited[0]
+        a = self.getLevel(root, x, 1)
+        b = self.getLevel(root, y, 1)
+        
+        if a == b:
+            if self.findParent(root, -1, x) != self.findParent(root, -1, y):
+                return True
+        return False
