6695b83f9c2fc058e521209a1d877a041da8f69c
Himanshu Patel <hpatel27299@gmail.com>
2020-06-02 15:09:03 +0530

    Day 2 - April LeetCoding

---
+++ April LeetCoding Challenge 2020/202-happy-number.py
@@ -0,0 +1,38 @@
+# Question: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3284/
+
+"""
+Write an algorithm to determine if a number n is "happy".
+A happy number is a number defined by the following process: 
+Starting with any positive integer, replace the number by the sum of the squares of its digits,
+and repeat the process until the number equals 1 (where it will stay), or it loops endlessly 
+in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+Return True if n is a happy number, and False if not.
+
+Example: 
+    Input: 19
+    Output: true
+    Explanation: 
+    12 + 92 = 82
+    82 + 22 = 68
+    62 + 82 = 100
+    12 + 02 + 02 = 1
+"""
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        nums = []
+        
+        def new_num(n):
+            sum = 0
+            while n:
+                i = n%10
+                n //= 10
+                sum += i**2
+            return sum
+        
+        while n != 1:
+            n = new_num(n)
+            if n in nums:
+                return False
+            nums.append(n)
+        return True
