d5f521e5604896dd933c04b637d6617818632365
Himanshu Patel <hpatel27299@gmail.com>
2020-06-08 15:06:52 +0530

    Day 8 - June LeetCoding

---
+++ June LeetCoding Challenge 2020/743-power-of-two.py
@@ -0,0 +1,31 @@
+# Question: https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3354/
+
+"""
+Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+    Input: 1
+    Output: true 
+    Explanation: 20 = 1
+
+Example 2:
+    Input: 16
+    Output: true
+    Explanation: 24 = 16
+
+Example 3:
+    Input: 218
+    Output: false
+"""
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        count = 0
+        flag = True
+        while n:
+            count += n&1
+            n = n>>1
+            if count > 1:
+                flag = False
+                break
+        return flag and count
