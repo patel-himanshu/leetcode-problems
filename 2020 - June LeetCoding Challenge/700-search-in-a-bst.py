cdfb3de1ec72fb41e7cd91d351c577176faea56c
Himanshu Patel <hpatel27299@gmail.com>
2020-06-15 21:29:53 +0530

    Day 15 - June LeetCoding

---
+++ 2020 - June LeetCoding Challenge/700-search-in-a-bst.py
@@ -0,0 +1,40 @@
+# Question: https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/541/week-3-june-15th-june-21st/3361/
+
+"""
+Given the root node of a binary search tree (BST) and a value. 
+You need to find the node in the BST that the node's value equals the given value. 
+Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+Example:
+    Given the tree:
+            4
+           / \
+          2   7
+         / \
+        1   3
+
+    And the value to search: 2
+    You should return this subtree:
+          2     
+         / \   
+        1   3
+
+    In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+
+Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root or root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
