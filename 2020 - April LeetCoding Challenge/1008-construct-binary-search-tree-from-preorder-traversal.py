e3d0180a7494e8a9f104288cf4e8bc28f3bdb2e7
Himanshu Patel <hpatel27299@gmail.com>
2020-06-20 22:46:08 +0530

    Day 20 - April LeetCoding

---
+++ 2020 - April LeetCoding Challenge/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -0,0 +1,52 @@
+# Question: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3305/
+
+"""
+Return the root node of a binary search tree that matches the given preorder traversal.
+(Recall that a binary search tree is a binary tree where for every node, any descendant of 
+node.left has a value < node.val, and any descendant of node.right has a value > node.val.
+Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+
+It's guaranteed that for the given test cases there is always possible to find a binary search tree with the given requirements.
+
+Example 1:
+    Input: [8,5,1,7,10,12]
+    Output: [8,5,10,1,7,null,12]
+                8
+               / \ 
+              5   10
+             / \    \ 
+            1   7    12
+
+Constraints:
+    (1) 1 <= preorder.length <= 100
+    (2) 1 <= preorder[i] <= 10^8
+    (3) The values of preorder are distinct.
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder):    # bstFromPreorder(self, preorder: List[int]) -> TreeNode
+        size = len(preorder)
+        root = TreeNode(preorder[0])
+        stack = [root]
+
+        for i in range(1, size):
+            temp = None
+
+            while stack and preorder[i] > stack[-1].val:
+                temp = stack.pop()
+            
+            if temp:
+                temp.right = TreeNode(preorder[i])
+                stack.append(temp.right)
+            else:
+                temp = stack[-1]
+                temp.left = TreeNode(preorder[i])
+                stack.append(temp.left)
+
+        return root
