df8c86388f824b9a9806cd1cc8e8281cf2d1dadb
Himanshu Patel <hpatel27299@gmail.com>
2020-07-05 23:36:14 +0530

    Day 26 - April LeetCoding

---
+++ 2020 - April LeetCoding Challenge/1143-longest-common-subsequence.py
@@ -0,0 +1,44 @@
+# Question: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3311/
+
+"""
+Given two strings text1 and text2, return the length of their longest common subsequence.
+A subsequence of a string is a new string generated from the original string with some characters (can be none) 
+deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). 
+A common subsequence of two strings is a subsequence that is common to both strings.
+If there is no common subsequence, return 0.
+
+Example 1:
+    Input: text1 = "abcde", text2 = "ace" 
+    Output: 3  
+    Explanation: The longest common subsequence is "ace" and its length is 3.
+
+Example 2:
+    Input: text1 = "abc", text2 = "abc"
+    Output: 3
+    Explanation: The longest common subsequence is "abc" and its length is 3.
+
+Example 3:
+    Input: text1 = "abc", text2 = "def"
+    Output: 0
+    Explanation: There is no such common subsequence, so the result is 0.
+
+Constraints:
+    (1) 1 <= text1.length <= 1000
+    (2) 1 <= text2.length <= 1000
+    (3) The input strings consist of lowercase English characters only.
+"""
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:       
+        def helper(text1, text2, i, j, memo):
+            if memo[i][j] < 0:
+                if i==len(text1) or j==len(text2):
+                    memo[i][j] = 0
+                elif text1[i] == text2[j]:
+                    memo[i][j] = 1 + helper(text1, text2, i+1, j+1, memo)
+                else:
+                    memo[i][j] = max(helper(text1, text2, i+1, j, memo), helper(text1, text2, i, j+1, memo))
+            return memo[i][j]
+        
+        memo = [[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        return helper(text1, text2, 0, 0, memo)
