bea7ee6fdb7fd1513f60398e6625e409a8f9e774
Himanshu Patel <hpatel27299@gmail.com>
2020-07-24 23:44:40 +0530

    Day 24 - July LeetCoding

---
+++ 2020 - July LeetCoding Challenge/797-all-paths-from-source-to-target.py
@@ -0,0 +1,33 @@
+# Question: https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3400/
+
+"""
+Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
+The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+
+Example:
+    Input: [[1,2], [3], [3], []] 
+    Output: [[0,1,3],[0,2,3]] 
+    Explanation: The graph looks like this:
+        0--->1
+        |    |
+        v    v
+        2--->3
+    There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+
+Note:
+    (1) The number of nodes in the graph will be in the range [2, 15].
+    (2) You can print different paths in any order, but you should keep the order of nodes inside one path.
+"""
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        def dfs(curr_node, path):
+            if curr_node == len(graph) - 1:
+                ans.append(path)
+            else:
+                for node in graph[curr_node]:
+                    dfs(node, path + [node])
+                
+        ans = []
+        dfs(0, [0])
+        return ans
