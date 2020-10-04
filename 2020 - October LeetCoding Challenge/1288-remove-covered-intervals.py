b5fae046665dec751cebb8bd2c72b312f224cba3
Himanshu Patel <hpatel27299@gmail.com>
2020-10-04 18:05:47 +0530

    Day 4 - October LeetCoding

---
+++ 2020 - October LeetCoding Challenge/1288-remove-covered-intervals.py
@@ -0,0 +1,52 @@
+# Question: https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3483/
+
+"""
+Given a list of intervals, remove all intervals that are covered by another interval in the list.
+Interval [a,b) is covered by interval [c,d) if and only if c <= a and b <= d.
+After doing so, return the number of remaining intervals.
+
+Example 1:
+    Input: intervals = [[1,4],[3,6],[2,8]]
+    Output: 2
+    Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+
+Example 2:
+    Input: intervals = [[1,4],[2,3]]
+    Output: 1
+
+Example 3:
+    Input: intervals = [[0,10],[5,12]]
+    Output: 2
+
+Example 4:
+    Input: intervals = [[3,10],[4,10],[5,11]]
+    Output: 2
+
+Example 5:
+    Input: intervals = [[1,2],[1,4],[3,4]]
+    Output: 1
+ 
+Constraints:
+    (1) 1 <= intervals.length <= 1000
+    (2) intervals[i].length == 2
+    (3) 0 <= intervals[i][0] < intervals[i][1] <= 10^5
+    (4) All the intervals are unique.
+"""
+
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key = lambda a: (a[0], -a[1]))
+        ans = 0
+        end = None
+        
+        for start, stop in intervals:
+            if not end:
+                end = stop
+                ans += 1
+            elif start < end and stop <= end:
+                continue
+            else:
+                end = stop
+                ans += 1
+        
+        return ans
